b1d2d4a93d8bcae104e96669927409ffac0cbd2a
shawlynot <88781957+shawlynot@users.noreply.github.com>
2025-04-27 21:05:52 +0100

    more stalwart

--- db/init.sh
+++ db/init.sh
@@ -1,19 +1,26 @@
 set -e
 
-psql -v ON_ERROR_STOP=1 -v "zitadelpass=$ZITADEL_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
+psql -v ON_ERROR_STOP=1 -v "zitadelpass=$ZITADEL_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -v "stalwart=$STALWART_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
     CREATE DATABASE zitadel;
     CREATE DATABASE therapy;
+    CREATE DATABASE stalwart;
 
     CREATE ROLE zitadel_role;
     GRANT ALL ON DATABASE zitadel to zitadel_role;
 
     CREATE ROLE therapy_role;
-    GRANT ALL ON DATABASE therapy to therapy_role;    
+    GRANT ALL ON DATABASE therapy to therapy_role;
+
+    CREATE ROLE stalwart_role;
+    GRANT ALL ON DATABASE stalwart to stalwart_role;    
 
     CREATE USER zitadel WITH PASSWORD :'zitadelpass';
     GRANT zitadel_role to zitadel;
 
     CREATE USER therapy WITH PASSWORD :'therapypass';
     GRANT therapy_role to therapy;
+
+    CREATE USER stalwart WITH PASSWORD :'stalwart';
+    GRANT stalwart_role to stalwart;
 EOSQL
 
