577d1506463f7423a6c514a098e88f05d68796db
shawlynot <88781957+shawlynot@users.noreply.github.com>
2025-05-08 20:59:45 +0100

    auth with authentik

--- db/init.sh
+++ db/init.sh
@@ -1,17 +1,17 @@
 set -e
 
-psql -v ON_ERROR_STOP=1 -v "zitadelpass=$ZITADEL_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
-    CREATE DATABASE zitadel;
+psql -v ON_ERROR_STOP=1 -v "authpass=$AUTH_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
+    CREATE DATABASE auth;
     CREATE DATABASE therapy;
 
-    CREATE ROLE zitadel_role;
-    GRANT ALL ON DATABASE zitadel to zitadel_role;
+    CREATE ROLE auth_role;
+    GRANT ALL ON DATABASE auth to auth_role;
 
     CREATE ROLE therapy_role;
     GRANT ALL ON DATABASE therapy to therapy_role;
 
-    CREATE USER zitadel WITH PASSWORD :'zitadelpass';
-    GRANT zitadel_role to zitadel;
+    CREATE USER auth WITH PASSWORD :'authpass';
+    GRANT auth_role to auth;
 
     CREATE USER therapy WITH PASSWORD :'therapypass';
     GRANT therapy_role to therapy;
