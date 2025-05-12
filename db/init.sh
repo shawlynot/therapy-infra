111b356b0934822697e32a2c38e3ab9afa8daffc
shawlynot <88781957+shawlynot@users.noreply.github.com>
2025-05-12 21:25:19 +0100

    no more self hosted auth

--- db/init.sh
+++ db/init.sh
@@ -1,15 +1,8 @@
-psql -v ON_ERROR_STOP=1 -v "authpass=$AUTH_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
-    CREATE DATABASE auth;
+psql -v ON_ERROR_STOP=1 -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
     CREATE DATABASE therapy;
-
-    CREATE ROLE auth_role;
-    ALTER DATABASE auth OWNER TO auth_role;
 
     CREATE ROLE therapy_role;
     ALTER DATABASE therapy OWNER TO therapy_role;
-
-    CREATE USER auth WITH PASSWORD :'authpass';
-    GRANT auth_role to auth;
 
     CREATE USER therapy WITH PASSWORD :'therapypass';
     GRANT therapy_role to therapy;
