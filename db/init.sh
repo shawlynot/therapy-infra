7c7e01e06c5205243e1533ed519b242f01a4761a
shawlynot <88781957+shawlynot@users.noreply.github.com>
2025-05-12 10:23:38 +0100

    db

--- db/init.sh
+++ db/init.sh
@@ -1,14 +1,12 @@
-set -e
-
 psql -v ON_ERROR_STOP=1 -v "authpass=$AUTH_PG_PASS" -v "therapypass=$THERAPY_PG_PASS" -U "$POSTGRES_USER" -d postgres   <<-EOSQL
     CREATE DATABASE auth;
     CREATE DATABASE therapy;
 
     CREATE ROLE auth_role;
-    GRANT ALL ON DATABASE auth to auth_role;
+    ALTER DATABASE auth OWNER TO auth_role;
 
     CREATE ROLE therapy_role;
-    GRANT ALL ON DATABASE therapy to therapy_role;
+    ALTER DATABASE therapy OWNER TO therapy_role;
 
     CREATE USER auth WITH PASSWORD :'authpass';
     GRANT auth_role to auth;
@@ -16,4 +14,3 @@ psql -v ON_ERROR_STOP=1 -v "authpass=$AUTH_PG_PASS" -v "therapypass=$THERAPY_PG_
     CREATE USER therapy WITH PASSWORD :'therapypass';
     GRANT therapy_role to therapy;
 EOSQL
-
--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -e 
 docker build -t shawlynot/db:1 db
 docker save -o db/build/db.tar shawlynot/db:1
